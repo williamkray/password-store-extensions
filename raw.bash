44bbbd2bad6a5f3775d5d9b89976cee743c4a9a3
William Kray <william@kray.ca>
2017-01-04 08:39:02 -0800

    credit where credit is due

--- raw.bash
+++ raw.bash
@@ -1,1 +1,2 @@
+## taken from Lenz Weber's suggestion in this email-list discussion: https://lists.zx2c4.com/pipermail/password-store/2016-November/002433.html
 { cd ${PASSWORD_STORE_DIR:-$HOME/.password-store}; find $1 -type f -name '*.gpg' -printf "%P\n" | sed 's/\.gpg$//'; }
