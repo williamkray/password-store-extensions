551691364e6f93a2d0c81f8356a83f3a5411444e
William Kray <william@kray.ca>
2017-01-04 08:35:07 -0800

    include comment

--- otp.bash
+++ otp.bash
@@ -1,1 +1,5 @@
+## assumes that:
+## a) otp.sh is in your path (https://github.com/williamkray/scripts/blob/master/otp.sh)
+## b) pass entry includes a line for otp secret token, like:
+##     otp: 1234568mysecretTOKEN
 otp.sh $(pass $@|grep otp|awk '{print $2}')
