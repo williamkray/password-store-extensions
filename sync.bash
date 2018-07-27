93243bae6752a5f2d657284f3d13f81b4972ec3f
William Kray <william@kray.pw>
2018-07-27 13:40:41 -0700

    fix sync so it always changes to password directory. todo: have that directory be more dynamic.

--- sync.bash
+++ sync.bash
@@ -1,6 +1,8 @@
 ## just does a pull and then a push to sync with remote git repo
+pushd $HOME/.password-store/
 echo "pulling from remote"
 git pull 
 echo "pushing to remote"
 git push
 echo "done"
+popd
