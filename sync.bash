f9e69dcba74054dddf6954b89ddc6996b93bfaab
William Kray <william@kray.pw>
2019-07-26 22:59:05 -0700

    better

--- sync.bash
+++ sync.bash
@@ -1,5 +1,5 @@
 ## just does a pull and then a push to sync with remote git repo
-pushd $HOME/.password-store/
+pushd ${PASSWORD_STORE_DIR-~/.password-store}
 echo "pulling from remote"
 git pull 
 echo "pushing to remote"
